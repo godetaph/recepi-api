a38bc9d602d90ff23fdcc04f8033e262110c0780
godetaph <godelta.ph@gmail.com>
2021-08-30 16:25:43 +0800

    Cleaned test models file

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-
 class ModelTests(TestCase):
     def test_create_user_with_email_successfully(self):
         """Test create user successfully"""
@@ -11,7 +10,6 @@ class ModelTests(TestCase):
             email=email,
             password=password
         )
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
@@ -19,7 +17,6 @@ class ModelTests(TestCase):
         """Normalize email test"""
         email='test@GMAIL.COM'
         user = get_user_model().objects.create_user(email, 'test1234')
-
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
@@ -27,13 +24,11 @@ class ModelTests(TestCase):
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, '12345')
 
-    
     def test_create_new_superuser(self):
         """Superuser test"""
         user = get_user_model().objects.create_superuser(
             'test@gmail.com',
             'testing123'
         )
-
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
